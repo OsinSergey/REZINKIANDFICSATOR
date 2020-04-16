f6534e8040fd20c63ef08eee5cee84f48ca1a075
Osin Sergey <osin_osin@mail.ru>
2020-04-16 22:00:47 +0600

    Add files via upload

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
+	let modalnav = $('.link-ham'),
+	over = $('.overline'),
+	closenav = $('.closed'),
+	btnNav = $('.menu-btn');
 
-	// Custom JS
+btnNav.click(function () {
+	over.show();
+	$('html, body').css({
+		overflow: 'hidden',
+		height: '100%'
+	});
+
+});
+modalnav.click(function () {
+	over.hide();
+	$('html, body').css({
+		overflow: 'auto',
+		height: 'auto'
+	});
+
+});
+closenav.click(function () {
+	over.hide();
+	$('html, body').css({
+		overflow: 'auto',
+		height: 'auto'
+	});
+
+});
 
 });
