248e7b5cb8a70146374183cb003d14616d4b26ec
Osin Sergey <osin_osin@mail.ru>
2020-04-17 00:22:18 +0600

    Add files via upload

--- js/_custom.js
+++ js/_custom.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+	function backToTop() {
+		let button = $('.back-to-top');
+
+		$(window).on('scroll', () => {
+			if ($(this).scrollTop() >= 400) {
+				button.fadeIn();
+
+			} else {
+				button.fadeOut();
+			}
+		});
+		button.on('click', (e) => {
+
+			e.preventDefault();
+			$('html').animate({
+				scrollTop: 0
+			}, 1000);
+		});
+
+	}
+	backToTop();
 	let modalnav = $('.link-ham'),
 	over = $('.overline'),
 	closenav = $('.closed'),
